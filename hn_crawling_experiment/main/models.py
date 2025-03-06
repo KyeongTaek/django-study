a73e68bfc3f17e5982f2be11ceda5a9c241b3271
KyeongTaek <tommyholic@naver.com>
2025-03-06 20:08:32 +0900

    Feat : create db
    created db by following the ref: https://softwaree.tistory.com/74
    
    Signed-off-by: KyeongTaek <tommyholic@naver.com>

--- hn_crawling_experiment/main/models.py
+++ hn_crawling_experiment/main/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class StoryData(models.Model):
+    by = models.CharField(max_length=20) # author
+    descendants = models.IntegerField() # num of comments
+    post_id = models.CharField(max_length=15) # story id
+    score = models.IntegerField() # num of views
+    time = models.DateTimeField() # time
+    title = models.CharField(max_length=300) # story title
+    type = models.CharField(max_length=15) # type(job, story, ...)
+    url = models.URLField() # link
+    content = models.TextField(max_length=100000) # story content
