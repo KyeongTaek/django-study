1093fa37a5968b1b7d64a11c73df329617581c06
KyeongTaek <tommyholic@naver.com>
2025-03-06 20:41:41 +0900

    Feat: Add function 'add_new_items'
    Add function 'add_new_items'
     - save data in db
     - able to view db at '/admin'
    
    Signed-off-by: KyeongTaek <tommyholic@naver.com>

--- hn_crawling_experiment/hacker_crawler.py
+++ hn_crawling_experiment/hacker_crawler.py
@@ -4,9 +4,16 @@ from urllib.parse import urlparse
 import requests
 from bs4 import BeautifulSoup
 
+# Ref : https://softwaree.tistory.com/74 [Owl Life:티스토리]
 from datetime import datetime
 
-# Ref : https://softwaree.tistory.com/74 [Owl Life:티스토리]
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', "hn_crawling_experiment.settings")
+
+import django
+
+django.setup()
+from main.models import StoryData
+
 def fetch_hacker_latest_id():
     result = []
 
@@ -32,7 +39,7 @@ def fetch_hacker_latest_id():
         score = specific_id['score']
 
         time_stamp = specific_id['time'] # timestamp format
-        time = datetime.fromtimestamp(time_stamp)  # from timestamp to datetime format
+        time = datetime.fromtimestamp(time_stamp)  # from timestamp to datetime format TODO: fix warning on 'naive datetime'
 
         title = specific_id['title']
         type = specific_id['type']
@@ -57,5 +64,33 @@ def fetch_hacker_latest_id():
         maximum_crawl = maximum_crawl - 1 # count
     return result
 
+def add_new_items(crawled_stories):
+    last_inserted_story = StoryData.objects.last()
+    if last_inserted_story is None:
+        last_inserted_id = ""
+    else:
+        last_inserted_id = getattr(last_inserted_story, 'id')
+
+    items_to_insert_into_db = []
+
+    for story in crawled_stories:
+        if story['id'] == last_inserted_id: # check if it is the end
+            break
+        items_to_insert_into_db.append(story)
+    items_to_insert_into_db.reverse() # new story at the top
+
+    for story in items_to_insert_into_db:
+        print("new story added!!! : " + str(story['id']) + '. ' + story['title'])
+
+        StoryData(by=story['by'],
+                  descendants=story['descendants'],
+                  post_id=story['id'],
+                  score=story['score'],
+                  time=story['time'],
+                  title=story['title'],
+                  url=story['url']).save()
+
+    return items_to_insert_into_db
+
 if __name__ == '__main__':
-    fetch_hacker_latest_id()
+    add_new_items(fetch_hacker_latest_id())
