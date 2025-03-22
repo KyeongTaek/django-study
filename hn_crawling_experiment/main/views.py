eeccc9101825260adf5172c4f7ea3b5b7a909a81
KyeongTaek <tommyholic@naver.com>
2025-03-22 23:50:15 +0900

    feat: Add login page
    Signed-off-by: KyeongTaek <tommyholic@naver.com>

--- hn_crawling_experiment/main/views.py
+++ hn_crawling_experiment/main/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'main/index.html')
+
+# login function that requests login.html
+def login(request):
+    return render(request, 'main/login.html')
