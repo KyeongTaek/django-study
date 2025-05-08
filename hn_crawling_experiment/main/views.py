2955f05e86c9c01d66ba816865bb14d68386e380
KyeongTaek <tommyholic@naver.com>
2025-05-08 13:02:17 +0900

    fix: fix .create()
    use .create() method in .create_post()
    
    Signed-off-by: KyeongTaek <tommyholic@naver.com>

--- hn_crawling_experiment/main/views.py
+++ hn_crawling_experiment/main/views.py
@@ -9,8 +9,8 @@ from rest_framework import viewsets
 
 # Create your views here.
 
-class PostViewSet(viewsets.ViewSet):
-    def retrieve(self, request, *args, **kwargs): # posting function that requests posting.html
+class PostViewSet(viewsets.ModelViewSet):
+    def retrieve(self, request): # posting function that requests posting.html
         instance = self.get_object()
         serializer = self.get_serializer(instance)
         if serializer.is_valid():
@@ -18,17 +18,22 @@ class PostViewSet(viewsets.ViewSet):
             return Response(serializer.data, status=201)
         return Response(serializer.errors, status=400)
 
-    def create(self, request): # new_post function that requests new_post.html
-        if request.method == 'POST':
-            new_article = JSONParser().parse(request)
-            serializer = PostSerializer(data=new_article)
-            if serializer.is_valid():
-                serializer.save()
-                return JsonResponse(serializer.data, status=201)
-            return JsonResponse(serializer.errors, status=400)
+    @action(detail=True, methods=['post'])
+    def create_post(self, request):
+        return create(request)
+
+    # def create(self, request): # new_post function that requests new_post.html
+    #     if request.method == 'POST':
+    #         new_article = JSONParser().parse(request)
+    #         serializer = PostSerializer(data=new_article)
+    #         if serializer.is_valid():
+    #             serializer.save()
+    #             return JsonResponse(serializer.data, status=201)
+    #         return JsonResponse(serializer.errors, status=400)
 
     def list(self, request): # index function that requests index.html
-        serializer = PostSerializer(data=request.data)
+        queryset = Post.objects.all()
+        serializer = PostSerializer(queryset, many=True)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=201)
