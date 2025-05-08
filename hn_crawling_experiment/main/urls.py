c54c60f414f4ec653ef9ec360a54fa1f98cd7fe7
KyeongTaek <tommyholic@naver.com>
2025-05-08 20:27:13 +0900

    refactor: refactor urls.py in drf form
    Signed-off-by: KyeongTaek <tommyholic@naver.com>

--- hn_crawling_experiment/main/urls.py
+++ hn_crawling_experiment/main/urls.py
@@ -1,17 +1,25 @@
 from django.urls import path
 from django.contrib import admin
 
-from main.views import index, posting, login, new_post
+from main.views import PostViewSet, login
 
 from django.conf.urls.static import static
 from django.conf import settings
 
+post_specific_view = PostViewSet.as_view({
+    'get': 'retrieve',
+    'post': 'create_post',
+})
+
+post_list_view = PostViewSet.as_view({
+    'get': 'list',
+})
 
 urlpatterns = [
-    path('', index, name='index'),
-    path('<int:pk>/',posting, name="posting"),
+    path('',post_list_view, name='index'),
+    path('<int:pk>/',post_specific_view, name="posting"),
     path('login/', login, name='login'),
-    path('new_post/', new_post, name='new_post'),
+    path('new_post/',post_specific_view, name='new_post'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
