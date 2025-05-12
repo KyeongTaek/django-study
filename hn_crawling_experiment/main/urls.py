a22f4db34b5d0f57bd54973752426daaf5e72b37
KyeongTaek <tommyholic@naver.com>
2025-05-12 16:41:40 +0900

    fix: fix attribute error by adding serializer_class
    Signed-off-by: KyeongTaek <tommyholic@naver.com>

--- hn_crawling_experiment/main/urls.py
+++ hn_crawling_experiment/main/urls.py
@@ -1,24 +1,23 @@
 from django.urls import path
 from django.contrib import admin
 
-from main.views import PostViewSet, login
+from .views import PostViewSet
 
 from django.conf.urls.static import static
 from django.conf import settings
 
 post_specific_view = PostViewSet.as_view({
-    'get': 'retrieve',
-    'post': 'create_post',
+    'get': 'retrieve', # GET /{id}
+    'post': 'create', # POST /new_post/
 })
 
 post_list_view = PostViewSet.as_view({
-    'get': 'list',
+    'get': 'list', # GET /
 })
 
 urlpatterns = [
     path('',post_list_view, name='index'),
     path('<int:pk>/',post_specific_view, name="posting"),
-    path('login/', login, name='login'),
     path('new_post/',post_specific_view, name='new_post'),
 ]
 
--- hn_crawling_experiment/main/views.py
+++ hn_crawling_experiment/main/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from .models import Post
-from django.core.paginator import Paginator
 
 from .serializers import PostSerializer
 from rest_framework.response import Response
@@ -10,36 +9,16 @@ from rest_framework import viewsets
 # Create your views here.
 
 class PostViewSet(viewsets.ModelViewSet):
-    def retrieve(self, request): # posting function that requests posting.html
-        instance = self.get_object()
-        serializer = self.get_serializer(instance)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=201)
-        return Response(serializer.errors, status=400)
-
-    @action(detail=True, methods=['post'])
-    def create_post(self, request):
-        return create(request)
-
-    # def create(self, request): # new_post function that requests new_post.html
-    #     if request.method == 'POST':
-    #         new_article = JSONParser().parse(request)
-    #         serializer = PostSerializer(data=new_article)
-    #         if serializer.is_valid():
-    #             serializer.save()
-    #             return JsonResponse(serializer.data, status=201)
-    #         return JsonResponse(serializer.errors, status=400)
+    serializer_class = PostSerializer
+    
+    def retrieve(self, request, pk=None): # posting function that requests posting.html
+        queryset = Post.objects.get(pk=pk)
+        serializer = self.get_serializer(queryset)
+        return Response(serializer.data)
 
     def list(self, request): # index function that requests index.html
-        queryset = Post.objects.all()
-        serializer = PostSerializer(queryset, many=True)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=201)
-        return Response(serializer.errors, status=400)
+        self.queryset = Post.objects.all()
+        serializer = self.get_serializer(self.queryset, many=True)
+        
 
-# login function that requests login.html
-def login(request):
-    return render(request, 'main/login.html')
-
+        return Response(serializer.data)
