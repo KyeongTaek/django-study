0b57b3f6677758c7a062fe25969d7cf6b2e837d3
KyeongTaek <tommyholic@naver.com>
2025-05-13 21:03:57 +0900

    style: refactor model.py
    delete unused model
    
    Signed-off-by: KyeongTaek <tommyholic@naver.com>

--- hn_crawling_experiment/main/admin.py
+++ hn_crawling_experiment/main/admin.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
-from main.models import StoryData
 from .models import Post
 
-admin.site.register(StoryData)
 admin.site.register(Post)
--- hn_crawling_experiment/main/models.py
+++ hn_crawling_experiment/main/models.py
@@ -2,20 +2,6 @@ from django.db import models
 
 # Create your models here.
 
-class StoryData(models.Model):
-    by = models.CharField(max_length=20) # author
-    descendants = models.IntegerField() # num of comments
-    post_id = models.CharField(max_length=15) # story id
-    score = models.IntegerField() # num of views
-    time = models.DateTimeField() # time
-    title = models.CharField(max_length=300) # story title
-    type = models.CharField(max_length=15) # type(job, story, ...)
-    url = models.URLField() # link
-    content = models.TextField(max_length=100000) # story content
-
-    def __str__(self):
-        return self.title
-
 class Post(models.Model):
     postname = models.CharField(max_length=50)
     mainphoto = models.ImageField(blank=True, null=True)
