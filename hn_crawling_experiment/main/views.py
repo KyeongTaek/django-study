b3561805bcb39c8ad0bd591e58fd8ecaf87fd38f
KyeongTaek <tommyholic@naver.com>
2025-05-28 21:39:04 +0900

    fix: Enable GET arguments in '/post'
    Signed-off-by: KyeongTaek <tommyholic@naver.com>

--- hn_crawling_experiment/main/views.py
+++ hn_crawling_experiment/main/views.py
@@ -11,8 +11,18 @@ from rest_framework import status
 # Create your views here.
 
 class PostViewSet(viewsets.ModelViewSet):
-    queryset = Post.objects.all()
     serializer_class = PostSerializer
+
+    def get_queryset(self):
+        queryset = Post.objects.filter(index=0) # 기본적으로는 index가 0인 애들(메인페이지용)
+        
+        weeks = self.request.query_params.get('weeks', None) # weeks가 인자로 들어왔으면 값, 안 들어왔으면 None
+        
+        if weeks is not None: # 인자 들어옴(/post?weeks='2025-5-4')
+            queryset = Post.objects.filter(weeks=weeks) # weeks가 '2025-5-4'인 애들
+
+        return queryset
+    
 
 class UserViewSet(mixins.CreateModelMixin, mixins.ListModelMixin, viewsets.GenericViewSet):
     serializer_class = UserSerializer
